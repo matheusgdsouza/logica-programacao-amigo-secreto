80f164b4fefbdc384ed32f0aebb4bacd66124299
Matheus de Souza <matheusgds3011@gmail.com>
2025-09-26 18:26:46 -0300

    Criando lista e adicionando nome digitado a lista

--- js/app.js
+++ js/app.js
@@ -1,8 +1,11 @@
+let listaNomes = [];
 function adicionar(){
     //Obter nome digitado pelo usuário
     let nomeDigitado = document.getElementById("nome-amigo").value;
 
     //Adicionar o nome em uma lista
+    listaNomes.push(nomeDigitado);
+
     //Exibir lista no campo de nomes
 }
 
