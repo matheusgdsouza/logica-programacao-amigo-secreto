3cc271ac84f87fde5a7bb8c588ec9305850a6f27
Matheus de Souza <matheusgds3011@gmail.com>
2025-09-26 18:23:41 -0300

    Obtendo nome digitado pelo usuario

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 function adicionar(){
     //Obter nome digitado pelo usuário
+    let nomeDigitado = document.getElementById("nome-amigo").value;
+
     //Adicionar o nome em uma lista
     //Exibir lista no campo de nomes
 }
